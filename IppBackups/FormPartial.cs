adfb26e4fb2a0f7e9019d8de115548e5d0f81f85
Femidigital <alfredoyefeso@googlemail.com>
2016-01-05 16:46:15 +0000

    Created a partial class for Form

---
+++ IppBackups/FormPartial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IppBackups
+{
+    public partial class Form1
+    {
+        private void InitializeCustomEvents()
+        {
+            this.rBtn_Restore.CheckedChanged += new System.EventHandler(this.rBtn_Restore_CheckedChanged);
+        }
+    }
+}
