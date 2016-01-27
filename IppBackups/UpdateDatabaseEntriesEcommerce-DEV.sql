2f713a59328b18024a26cc62293466c9ec23912d
Femidigital <alfredoyefeso@googlemail.com>
2016-01-27 17:12:30 +0000

    UpdateEcommerceDev script

---
+++ IppBackups/UpdateDatabaseEntriesEcommerce-DEV.sql
@@ -0,0 +1,13 @@
+﻿USE [TEST-Ecommerce]
+
+UPDATE [TEST-Ecommerce].[dbo].[Application]
+SET ApplicationPrefix = 'BDEV'
+WHERE [id] = 1
+
+UPDATE [TEST-Ecommerce].[dbo].[Application]
+SET ApplicationPrefix = 'SDEV'
+WHERE [id] = 2
+
+UPDATE [TEST-Ecommerce].[dbo].[Application]
+SET ApplicationPrefix = 'MDEV'
+WHERE [id] = 3
