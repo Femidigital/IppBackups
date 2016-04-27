6c9f9b10ccfa7bdbde218baf7d7253c884613a5d
Femidigital <alfredoyefeso@googlemail.com>
2016-04-27 14:07:27 +0100

    Still in progress...

--- IppBackups/DatabaseUpdates.cs
+++ IppBackups/DatabaseUpdates.cs
@@ -720,11 +720,16 @@ namespace IppBackups
             
             XDocument xdoc = XDocument.Load(sXmlFile);
 
+            var nTokens = doc.SelectSingleNode("Databases/Database[@name='" + cur_database + "']/Tables/Table[@name='" + tblName +"']/Environments/Environment[@name='" + cur_environment +"']/Tokens"); 
+            
+
             var tokens = from token in xdoc.Elements("Database")
                          let table = (string)token.Element("Table").Attribute("name")
                          let env = (string)token.Element("Table").Element("Environment").Attribute("name")
-                         where (table != null && table == tblName) && (env != null && env == cur_environment)
+                         //where (table != null && table == tblName) && (env != null && env == cur_environment)
+                         where ((string)token.Element("Database").Attribute("name") == cur_database) && ((string)token.Element("Table").Attribute("name") == tblName) && ((string)token.Element("Environment").Attribute("name") == cur_environment)
                          select token;
+            tokens.ToList().ForEach(x => x.Remove());
 
             /* where (string)token.Element("Table").Element("Environment").Element("ReplaceToken") == ""
              * var q = from node in xdoc.Descendants("Database")
@@ -740,7 +745,7 @@ namespace IppBackups
             
 
             // Update the script file.
-            SaveScriptFile();
+            //SaveScriptFile();
         }
 
         private void newRowBtn_Click(object sender, EventArgs e)
